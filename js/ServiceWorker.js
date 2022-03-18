a2803dc0e74f34244d0d6dea7947076964a46d95
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-18 12:26:37 +0100

    Update ServiceWorker.js

--- js/ServiceWorker.js
+++ js/ServiceWorker.js
@@ -11,7 +11,7 @@ self.addEventListener('activate', evt => {
 //fetch event afin de répondre quand on est en mode hors ligne.
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-    caches.open('ma_sauvegarde').then(function(cache) {
+    caches.open('my_save').then(function(cache) {
     return cache.match(event.request).then(function (response) {
     return response || fetch(event.request).then(function(response) {
     cache.put(event.request, response.clone());
