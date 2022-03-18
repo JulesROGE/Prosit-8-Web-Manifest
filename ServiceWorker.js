8cab1f59f60f46f70a05c501f738a8d7a6ebd871
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-18 11:52:55 +0100

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,2 +1,5 @@
 //Installation du service worker
 self.addEventListener('install', evt => {  });
+self.addEventListener('activate', evt => {
+    console.log('le Service Worker a été installé ');
+    });
